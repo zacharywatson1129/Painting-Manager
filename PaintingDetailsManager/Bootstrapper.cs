3042d803d1844e31abc4e9f4f59f98756f5425b5
Zachary Watson <zacharywatson1129@gmail.com>
2024-07-12 04:42:41 -0500

    Working on implementing Dependency Injection

--- PaintingDetailsManager/Bootstrapper.cs
+++ PaintingDetailsManager/Bootstrapper.cs
@@ -11,14 +11,45 @@ namespace PaintingDetailsManager
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            // container holds an instance of itself to pass out
+            // when we ask for a container.
+            _container.Instance(_container);
+
+            // Two important Caliburn Micro entities we may need to
+            // new up instances of.
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<MainWindowViewModel>();           
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }

7f91af23d61ac9075813f5d3f2ac13ff5b35b9d6
Zachary Watson <zacharywatson1129@gmail.com>
2024-07-12 22:04:17 -0500

    Rest of dependency injection wired up for our basic setup for now.

--- PaintingDetailsManager/Bootstrapper.cs
+++ PaintingDetailsManager/Bootstrapper.cs
@@ -25,10 +25,28 @@ namespace PaintingDetailsManager
             _container.Instance(_container);
 
             // Two important Caliburn Micro entities we may need to
-            // new up instances of.
+            // new up instances of. Could be more in future, but this
+            // is a start.
+            // This creates ONE instance for the entire application.
+            // We don't want to do this for everything, but we really
+            // only need ONE window manager and one event aggregator.
             _container
                 .Singleton<IWindowManager, WindowManager>()
                 .Singleton<IEventAggregator, EventAggregator>();
+
+            // Don't want to use a reflection a lot because of performance
+            // but it won't hurt to do it once at startup.
+            // Get every type in our application, then we filter it down.
+            // We only want to get classes whose name ends in ViewModels
+            // (we only want ViewModels, in other words).
+            // We turn that into a list, then ForEach type, we 
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+
 
         }
 
